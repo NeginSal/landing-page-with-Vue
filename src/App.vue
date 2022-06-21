71104003384eea9111cba7a88663a138d32f6b0a
NeginSal <neginsaljooghi@gmail.com>
2022-06-21 17:19:08 +0430

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+ <router-view />
+</template>
+
+<script>
+
+export default {
+  components: {},
+};
+</script>
+<style>
+body {
+  background-color: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #2c3e50;
+}
+</style>
