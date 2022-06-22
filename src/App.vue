d21e2b2f5f7c791415f1de80698dd576287c4590
NeginSal <neginsaljooghi@gmail.com>
2022-06-22 11:45:55 +0430

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
+<Navbar/>
  <router-view />
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 
 export default {
-  components: {},
+  components: {
+    Navbar
+  },
 };
 </script>
 <style>
@@ -16,8 +20,6 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  max-width: 600px;
-  margin: 0 auto;
   color: #2c3e50;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- <nav class="nav">
+    <ul>
+      <li>اتراکس</li>
+      <li>خانه</li>
+      <li>خرید رمز ارز</li>
+      <li>بازارها</li>
+      <li>تیدیل رمز ازز</li>
+      <li>بلاگ</li>
+      <li>ثبت نام</li>
+      <li>ورود</li>
+    </ul>
+  </nav> -->
+  <nav id="navbar">
+    <ul class="one">
+      <li><a href="#">درباره ما</a></li>
+      <li><a href="#">تماس با ما</a></li>
+    </ul>
+    <ul class="two">
+      <li><a href="#">خانه</a></li>
+      <li><a href="#">درباره ما</a></li>
+      <li><a href="#">تماس با ما</a></li>
+      <li><a href="#">تماس با ما</a></li>
+      <li><a href="#">تماس با ما</a></li>
+      <li><a href="#">تماس با ما</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#navbar {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  position: sticky;
+  top: 0%;
+  background-color: white;
+}
+#navbar a {
+  color: black;
+}
+#navbar ul {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  list-style: none;
+}
+#navbar ul li a {
+  padding: 0.75rem;
+  margin: 0 0.25rem;
+}
+#navbar ul li a:hover {
+  background: orange;
+  color: white;
+  border-radius: 5px;
+}
+</style>
