0e1f5229934948b645cb07a35e6d8124b1dc7091
NeginSal <neginsaljooghi@gmail.com>
2022-06-22 15:35:12 +0430

    add section component

---
+++ src/components/Section.vue
@@ -0,0 +1,65 @@
+<template>
+    <section id="section2">
+        <div class="content">
+            <h2 class="m-heading m-2">لورم ایپسوم متن ساختگی</h2>
+            <p class="my-1">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+            </p>
+            <h3>لورم ایپسوم</h3>
+            <ul class="list">
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+            </ul>
+        </div>
+        <div class="content bg-dark p-2">
+            <h2 class="m-heading m-2">لورم ایپسوم متن ساختگی</h2>
+            <p class="my-1">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+            </p>
+            <h3>لورم ایپسوم</h3>
+            <ul class="list">
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+                <li>لورم ایپسوم</li>
+            </ul>
+        </div>
+    </section>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#section2{
+    display: flex;
+}
+#section2 div{
+    flex: 1;
+}
+
+#section2 .content{
+    text-align: right;
+}
+.list{
+    margin: 0.50rem;
+    list-style: none;
+}
+.list li{
+    border-bottom: #444 dotted 1px;
+    padding: 0.5rem 0;
+}
+</style>>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <Table/>
+    <Section/>
+    <Section/>
+    <Section/>
   </div>
 </template>
 
 <script>
 import Table from '../components/Table.vue'
+import Section from '../components/Section.vue'
+
 export default {
   name: "Home",
   components: {
-    Table
+    Table,
+    Section
   },
   data() {
     return {};
