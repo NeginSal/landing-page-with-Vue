316df58d599196a1eb69e00b904546348487624a
NeginSal <neginsaljooghi@gmail.com>
2022-06-22 19:22:12 +0430

    edit footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,105 +1,127 @@
 <template>
-   <footer id="main-footer" class="py-2">
-      <div class="container footer-container">
-        <div>
-          <h3>راهنمای خرید</h3>
-          <ul class="list">
-            <li><a href="#"> مقالات </a></li>
-            <li><a href="#"> برترین های </a></li>
-            <li><a href="#"> درباره ما </a></li>
-            <li><a href="#"> تماس با ما</a></li>
-          </ul>
+  <footer>
+    <div id="footer1">
+      <ul>
+        <li><a href="#"> دانلود مستقیم</a></li>
+        <li><a href="#">Play Store</a></li>
+      </ul>
+      <p class="logo">
+        <a href="#"
+          ><span> .با اپلیکیشن اتراکس در هر لحظه خرید و فروش کن </span></a
+        >
+      </p>
+    </div>
+    <section id="section1">
+        <div class="items">
+          <div class="item">
+            <div>
+              <p class="title">راهنماو پشتیبانی</p>
+              <ul>
+                <li>سوالات متداول</li>
+                <li>کارمزدها</li>
+                <li>احراز هویت و سطح کاربری</li>
+                <li>راهنمای امنیت حساب</li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+            <div>
+              <p class="title">درباره صرافی اتراکس</p>
+              <ul>
+                <li>درباره ما</li>
+                <li>تماس با ما</li>
+                <li>امنیت مجموعه</li>
+                <li>قوانین و مقررات</li>
+              </ul>
+            </div>
+          </div>
+          <div class="item">
+            <div>
+              <p>اتراکس</p>
+              <p>
+                برای حرکت به سوی اهداف و ماموریتمان، ما سه ارزش ساده را دنبال می
+                کنیم: برای جامعه مفید باشیم، مشتری مدار باشیم، قهرمان باشیم.
+              </p>
+              <p>icona</p>
+            </div>
+          </div>
         </div>
 
-        <div>
-          <h3>لینک های سایت</h3>
-          <ul class="list">
-            <li><a href="#"> مقالات </a></li>
-            <li><a href="#"> برترین های </a></li>
-            <li><a href="#"> درباره ما </a></li>
-            <li><a href="#"> تماس با ما</a></li>
-          </ul>
-        </div>
-
-        <div>
-          <h2>عضو شوید</h2>
-          <p>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-            استفاده از طراحان گرافیک است.
-          </p>
-          <a href="#" class="btn btn-secondary"> عضویت </a>
-        </div>
-
-        <div>
-          <h2 class="logo"><span class="text-primary">WEBPROG</span>.ir</h2>
-          <p>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-            استفاده از طراحان گرافیک است.
-          </p>
-        </div>
-        
-        <div>
-          <p>
-            تمامی حقوق این سایت محفوظ و متعلق به اِتِرِکس می باشد 
-          </p>
-        </div>
-      </div>
-    </footer>
-  
+    </section>
+    <div id="main-footer">
+      <p>تمامی حقوق این سایت محفوظ و متعلق به اِتِرِکس می باشد</p>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-h3{
-  color: #2A6AE3;
-  font-weight:700 ;
-  font-size: 18px;
+#footer1 {
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
 
+  margin: 10px;
+}
+#footer1 a {
+  color: #3c3c3c;
+  text-decoration: none;
+}
+#footer1 ul {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  list-style: none;
+}
+#footer1 ul li a {
+  padding: 16px 24px;
+  margin: 0 0.25rem;
+  font-size: 16px;
+  color: #ffffff;
+  background: #2a6ae3;
+  border-radius: 20px;
+}
+.logo {
+  font-size: 20px;
+  font-weight: 700;
 }
 #main-footer {
-  background: var(--dark-color);
-  color: black;
-}
-
-#main-footer a {
-  color: black;
-}
-
-#main-footer .footer-container {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 1.5rem;
-}
-
-#main-footer .footer-container div:last-child {
-  background: #2A6AE3;
+  color: #ffffff;
+  background: #2a6ae3;
   border-radius: 32px 32px 0px 0px;
-  color: white;
-  font-size: 16px;
+  text-align: center;
   font-weight: 400;
-  text-align: center;
-  grid-column: 1 / span 4;
-  padding: .5rem;
-  font-size: 0.75rem;
+  font-size: 16px;
+  padding: 10px;
 }
-
-#main-footer .footer-container div:last-child p{
-  text-align: center;
+#section1 {
+  background: #f2f7ff;
+  border-radius: 32px 32px 0px 0px;
+  color: #000000;
 }
-
-#main-footer .footer-container input[type='email'] {
-  width: 90%;
-  padding: 0.5rem;
-  margin-bottom: 0.5rem;
+#section1 .items {
+  display: flex;
+  padding: 15px;
 }
-
-#main-footer .footer-container input[type='submit'] {
-  width: 90%;
+#section1 .items .item {
+  flex: 1;
+  text-align: right;
+  padding: 15px;
 }
-
+#section1 ul li {
+  text-align: right;
+  list-style: none;
+  font-weight: 500;
+  font-size: 16px;
+  padding: 10px;
+}
+.title {
+  font-weight: 700;
+  font-size: 18px;
+  color: #2a6ae3;
+  text-align: right;
+}
 </style>
